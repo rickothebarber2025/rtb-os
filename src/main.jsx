5fc12de2e4c182ef6ada6f95b07538dbfb91bf9b
rickothebarber2025 <rickothebarber@gmail.com>
2026-08-27 12:11:21 -0400

    Use one native owner access system

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx';
 import ErrorBoundary from './components/ErrorBoundary.jsx';
 import MobileNavigationEnhancer from './components/MobileNavigationEnhancer.jsx';
 import StaffHubNavigationEnhancer from './components/StaffHubNavigationEnhancer.jsx';
-import OwnerRoleAccessSetup from './components/OwnerRoleAccessSetup.jsx';
 import { AuthProfileProvider } from './contexts/AuthProfileContext.jsx';
 import './styles/global.css';
 import './styles/mobileNavigation.css';
@@ -17,6 +16,7 @@ import './styles/humanExperience.css';
 import './styles/integrations.css';
 import './styles/staffHubHumanNav.css';
 import './styles/staffHubVibes.css';
+import './styles/accessControl.css';
 
 const CACHE_CLEANUP_KEY = 'rtb-os-cache-cleanup-2026-07-12';
 
@@ -55,7 +55,6 @@ createRoot(document.getElementById('root')).render(
       <AuthProfileProvider>
         <MobileNavigationEnhancer />
         <StaffHubNavigationEnhancer />
-        <OwnerRoleAccessSetup />
         <App />
       </AuthProfileProvider>
     </ErrorBoundary>
