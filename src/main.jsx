26e36a0369801836bed2748702f3434d24a1a33e
rickothebarber2025 <rickothebarber@gmail.com>
2026-09-05 11:32:10 -0400

    Remove legacy Staff Hub DOM tab enhancer

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import ErrorBoundary from './components/ErrorBoundary.jsx';
 import MobileNavigationEnhancer from './components/MobileNavigationEnhancer.jsx';
-import StaffHubNavigationEnhancer from './components/StaffHubNavigationEnhancer.jsx';
 import { AuthProfileProvider } from './contexts/AuthProfileContext.jsx';
 import './styles/global.css';
 import './styles/mobileNavigation.css';
@@ -55,7 +54,6 @@ createRoot(document.getElementById('root')).render(
     <ErrorBoundary>
       <AuthProfileProvider>
         <MobileNavigationEnhancer />
-        <StaffHubNavigationEnhancer />
         <App />
       </AuthProfileProvider>
     </ErrorBoundary>
